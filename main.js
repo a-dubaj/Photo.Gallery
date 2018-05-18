81ab8edb00ba40868539c98d34ce1d08267be3ba
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2018-05-19 00:52:33 +0200

    Add changes: * renamed comments

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+// Looping through images 
 for (var i = 1; i <= 5; i++) {
     var newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/pic' + i + '.jpg');
@@ -19,7 +19,7 @@ function displayImage(imgSrc) {
     displayedImage.setAttribute('src', imgSrc);
 }
 
-/* Wiring up the Darken/Lighten button */
+// Wiring up the Darken/Lighten button
 btn.onclick = function() {
     var btnClass = btn.getAttribute('class');
     if (btnClass === 'dark') {
